f1057200c599a58b22151c8a91613e80014d4a15
Yovvel <joffrey.nolting@gmail.com>
2021-12-30 08:25:28 +0100

    created quotes.py

---
+++ quotes.py
@@ -0,0 +1,7 @@
+import random
+
+list = ["Readers are leaders. \nThe more you learn, the more you earn.",
+        "\n“Be tolerant with others and strict with yourself”\n\n                            --Marcus Aurelius\n", 
+        "\n“You have power over your mind - not outside events. Realize this, and you will find strength.”\n\n   --Marcus Aurelius\n"]
+item = random.choice(list)						# Chooses from list
+print(item)					# Prints choice
