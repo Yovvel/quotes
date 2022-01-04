c7814463e9e46106b5019667ad54c0f1eaf711cd
Yovvel <joffrey.nolting@gmail.com>
2022-01-04 14:52:59 +0100

    cleaned up

--- quotes.py
+++ quotes.py
@@ -1,7 +1,10 @@
 import random
-
-list = ["Readers are leaders. \nThe more you learn, the more you earn.",
-        "\n“Be tolerant with others and strict with yourself”\n\n                            --Marcus Aurelius\n", 
-        "\n“You have power over your mind - not outside events. Realize this, and you will find strength.”\n\n   --Marcus Aurelius\n"]
+    
+list = ["\nReaders are leaders. \nThe more you learn, the more you earn.",
+        "\n“Be tolerant with others and strict with yourself”\n\n                            --Marcus Aurelius", 
+        "\n“You have power over your mind - not outside events. Realize this, and you will find strength.”\n\n   --Marcus Aurelius",
+        "\n“The happiness of your life depends upon the quality of your thoughts.”\n\n   --Marcus Aurelius"]
 item = random.choice(list)						# Chooses from list
+
 print(item)					# Prints choice
+print("--------------------------------------------------------------------------------------------------------------\n")
